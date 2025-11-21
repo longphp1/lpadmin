66c7a5cdf27bf969d5f96ab3f2094f581a22a37b
longjun.he <2209442773@qq.com>
2025-11-21 16:51:48 +0800

    1

--- start.bat
+++ start.bat
@@ -2,17 +2,17 @@
 
 git pull
 
-:: 更新代码
+
 php artisan generate:website_new
 
-:: 更新weidian代码
+
 php artisan generate:weidian_site
 
-:: 推送代码
+
 php artisan push:website
 
-:: 推送weidian代码
+
 php artisan push:weidian
 
-:: 等待用户按键退出
+
 pause
